3f053c9126df603fb48fcb0851c04eb24953b531
Luca Quatela <lucaqq@gmail.com>
2016-04-01 09:20:20 -0700

    add the function to change the number into the words PING, PONG and PINGPONG

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,6 @@
 /* businnes logic START */
 
-var userInput = 0;
-var arrayNumUpToUserInput = [];
+/* this following function creates an array of numbers up to the one inserted by the user */
 
 var contToUserInput = function(numberToReach){
   var arrayNum = [];
@@ -11,17 +10,35 @@ var contToUserInput = function(numberToReach){
   return arrayNum;
 }
 
+/* this following function replace certain numbers with the words Ping and Pong */
+
+var pingPongFunction = function(arrayToModify){
+  indexPosition = 0;
+  arrayToModify.forEach(function(numberToCheck){
+  if (numberToCheck % 15 === 0) {
+    arrayToModify[indexPosition] = "pingpong";
+  } else if (numberToCheck % 5 === 0) {
+    arrayToModify[indexPosition] = "pong";
+  } else if (numberToCheck % 3 === 0) {
+    arrayToModify[indexPosition] = "ping";
+  }
+  indexPosition ++;
+  });
+  return arrayToModify;
+};
+
 /* businnes logic END */
 
-/* user logic START */
+/* user interface logic START */
 
 $(document).ready(function(){
   $("form").submit(function(event){
     event.preventDefault();
-    userInput = (parseInt($("#userInput").val()));
-    arrayNumUpToUserInput = (contToUserInput(userInput));
-    console.log(arrayNumUpToUserInput);
+    var userInput = (parseInt($("#userInput").val()));
+    var arrayNumUpToUserInput = (contToUserInput(userInput));
+    var arrayNumPingPonged = (pingPongFunction(arrayNumUpToUserInput));
+    console.log(arrayNumPingPonged);
   })
 });
 
-/* user logic END */
+/* user interface logic END */
